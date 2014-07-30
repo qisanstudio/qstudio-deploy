e5a51630a3e07028c52d214e1a4f38c3f241769c
qisan <yimiqisan@gmail.com>
2014-07-30 18:19:03 +0800

    add

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -3,13 +3,15 @@
 import os
 import fabtools
 from fabtools import require, python_setuptools
-from fabric.api import env
+from fabric.api import env, local, run, sudo
+from fabric.context_managers import prefix
+
 
 
 env.hosts = ['microsite']
-env.user = 'root'
-env.password = 'ckcenmgqqcoc'
-#env.key_filename = '~/.ssh/id_rsa_iefschina'
+env.user = 'qstudio'
+env.key_filename = '~/.ssh/id_rsa_droplet'
+env.warn_only = True
 
 PUB_KEY = os.path.join(os.path.abspath('.'), 'keys/id_rsa_droplet.pub')
 
@@ -25,34 +27,35 @@ def init_user(name):
         require.users.sudoer(name)
 
 
-def install_server():
-    '''
-        install server
-    '''
-    require.deb.packages([
-        'gcc',
-        'g++',
-        'git',
-        'libjansson-dev',
-        'python2.7',
-        'python2.7-dev',
-        'python-pip',
-        'build-essential',
-        'ruby',
-        'libpcre3',
-        'libpcre3-dev',
-        'postgresql-9.3'
-    ])
+def init_os():
+    with prefix("sudo aptitude update"):
+        sudo("aptitude -y upgrade")
+        sudo("aptitude install -y gcc")
+        sudo("aptitude install -y g++")
+        sudo("aptitude install -y git")
+        sudo("aptitude install -y libjansson-dev")
+        sudo("aptitude install -y python2.7")
+        sudo("aptitude install -y python2.7-dev")
+        sudo("aptitude install -y ruby")
+        sudo("aptitude install -y libpcre3")
+        sudo("aptitude install -y libpcre3-dev")
 
 
 def init_pyenv():
     '''
         init python envirment
     '''
+    sudo("aptitude install -y python-pip")
+    sudo("aptitude install -y build-essential")
+    if not python_setuptools.is_setuptools_installed():
+        python_setuptools.install_setuptools()
+    python_setuptools.install('virtualenv', use_sudo=True)
+    print '================================'
     python_setuptools.install([
-        'vritualenv',
         'virtualenvwrapper',
         'pep8',
         'pyfakes',
-        'flake8',
+        'flake8'
     ], use_sudo=True)
+
+
